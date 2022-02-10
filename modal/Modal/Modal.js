d7ba428b2975dd8d3c24bec6cbc9d4a7be22da53
Chanchal Panpaliya <chanchal.panpaliya@gmail.com>
2022-02-11 01:08:48 +0530

    modal comp commit
    commit modal component

---
+++ modal/Modal/Modal.js
@@ -0,0 +1,15 @@
+function Open_SimpleModal(){
+    document.getElementById("SimpleModal").style.display = "block"
+}
+
+function Close_SimpleModal(){
+    document.getElementById("SimpleModal").style.display = "none" 
+}
+
+function Open_ActionModal(){
+    document.getElementById("ActionModal").style.display = "block"
+}
+
+function Close_ActionModal(){
+    document.getElementById("ActionModal").style.display = "none" 
+}
