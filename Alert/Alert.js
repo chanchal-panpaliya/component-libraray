eb5cb55b28be9753f4b19352123af198733c252a
Chanchal Panpaliya <chanchal.panpaliya@gmail.com>
2022-02-15 12:57:14 +0530

    removed id , z-index , add alert utility class (separate)
    removed id , z-index , added alert utility class - bordercolor , bgcolor , color (update css code).JS code update.

--- Alert/Alert.js
+++ Alert/Alert.js
@@ -1,21 +1,21 @@
-var showalert_example = document.getElementById("showalert_example");
 
-showalert_example.addEventListener("click",()=>{
-    document.getElementById("alert_example").style.display = "block"
+/* for live example - close alert*/
+const close_alert_example = document.querySelector('.close_alert');
+
+close_alert_example.addEventListener("click",()=>{
+    document.querySelector('.alert_example').style.display = "none"
 })
 
-var close_alert = document.getElementById("close_alert");
-
-close_alert.addEventListener("click",()=>{
-     document.getElementById("alert_example").style.display = "none"
-
-})
+/* for live example - open alert*/
+function showalertbox(){
+  document.querySelector('.alert_example').style.display = "block"
+}
 
 
+
+/* for live example - group of action alert close*/
 var close = document.getElementsByClassName("close_action_alert");
-
-var i;
-for (i = 0; i < close.length; i++) {
+for (var i = 0; i < close.length; i++) {
   close[i].onclick = function(){
     var div = this.parentElement;
     div.style.opacity = "0";
@@ -24,16 +24,11 @@ for (i = 0; i < close.length; i++) {
 }
 
 
-
+/* for live example - group of action alert reload*/
 function reload_action_alert(){
   var reload = document.getElementsByClassName("action_alert");
-
-  var j;
-   for (j = 0; j < close.length; j++) {
- 
-  reload[j].style.opacity = "1";
-  reload[j].style.display = "flex"
-  
-}
-
+   for (var j = 0; j < close.length; j++) {
+    reload[j].style.opacity = "1";
+    reload[j].style.display = "flex"
+  }
 }
