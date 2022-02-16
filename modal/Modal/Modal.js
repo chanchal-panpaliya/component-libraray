075ac301dabb24fa69f913badf6f556534fd78dc
Chanchal Panpaliya <chanchal.panpaliya@gmail.com>
2022-02-16 13:40:15 +0530

    updated css and iframe added for css class.
    updated css and iframe added for css class.

--- modal/Modal/Modal.js
+++ modal/Modal/Modal.js
@@ -1,15 +1,15 @@
 function Open_SimpleModal(){
-    document.getElementById("SimpleModal").style.display = "block"
+    document.querySelector(".SimpleModal").style.display = "block"
 }
 
 function Close_SimpleModal(){
-    document.getElementById("SimpleModal").style.display = "none" 
+    document.querySelector(".SimpleModal").style.display = "none" 
 }
 
 function Open_ActionModal(){
-    document.getElementById("ActionModal").style.display = "block"
+    document.querySelector(".ActionModal").style.display = "block"
 }
 
 function Close_ActionModal(){
-    document.getElementById("ActionModal").style.display = "none" 
+    document.querySelector(".ActionModal").style.display = "none" 
 }
