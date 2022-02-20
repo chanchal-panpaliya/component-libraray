7be9ae580cb03c5db8da478189ff5e6fde5209c9
Chanchal Panpaliya <chanchal.panpaliya@gmail.com>
2022-02-21 00:15:26 +0530

    deleted all files
    deleted all files

--- components/Navigation/Navigation.js
+++
@@ -1,9 +0,0 @@
-function Open_desktop_NavBar(){
-    var x = document.getElementsByClassName("desktop-nav-bar-list")[0];
-    if (x.className === "desktop-nav-bar-list") {
-        x.className += " responsive";
-      } else {
-        x.className = "desktop-nav-bar-list";
-      }
-
-}
